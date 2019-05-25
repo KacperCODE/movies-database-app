d8d9b122857b27c0ab3e3300253db956e09feeff
Kacper <Acymon@gmail.com>
2019-05-26 00:28:51 +0200

    Working on Heroku v3

--- src/app/domain/actor.ts
+++ src/app/domain/actor.ts
@@ -1,10 +1,10 @@
 export class Actor {
-    birthday: string;
-    country: string;
-    gender: string;
-    imdbId: string;
-    name: string;
-    photoUrl: string;
-    __v: number;
-    _id: string;
+    public birthday: string;
+    public country: string;
+    public gender: string;
+    public imdbId: string;
+    public name: string;
+    public photoUrl: string;
+    public __v: number;
+    public _id: string;
 }
